041c6c4e030a9f7ab9bfa396e94b1dd4b38d338c
Simarbir2112006 <simarbir.sandhu@gmail.com>
2026-03-18 05:31:12 +0100

    Added src/interpret.py

---
+++ src/interpret.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import shap
+
+def plot_forecast(y_true, y_pred, n_steps=336):
+    results = pd.DataFrame(index=y_true.index)
+    results["Actual (MW)"] = y_true
+    results["Predicted (MW)"] = y_pred
+
+    results.iloc[:n_steps].plot(
+        y=["Actual (MW)", "Predicted (MW)"],
+        figsize=(10, 8),
+        title="First Week Forecast (Actual vs Predicted)",
+        color=["black", "red"],
+    )
+    plt.ylabel("Total Demand (MW)")
+    plt.xlabel("Date")
+    plt.grid(True, which='both', linestyle='--', linewidth=0.5)
+    plt.tight_layout()
+    plt.show()
+
+
+def shap_summary(model, X_test, n_samples=2000):
+    shap.initjs()
+    X_sample = X_test.sample(n_samples, random_state=42)
+    explainer = shap.TreeExplainer(model.model_2)
+    shap_values = explainer.shap_values(X_sample)
+    shap.summary_plot(shap_values, X_sample, plot_type="dot")
